289a14c815504867e324a1b56d3f8f348f6af5c5
elvodqa <emircengiz21@gmail.com>
2023-11-15 14:45:34 +0300

    Sexy scene info

--- VulkanEngine/Game.cs
+++ VulkanEngine/Game.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Numerics;
 using ImGuiNET;
 
 namespace VulkanEngine;
@@ -21,9 +22,8 @@ public static class Game
             VKRender.UpdateTime();
             var fps=fpsCounter.AddAndGetFrame();
             VKRender.imGuiController.Update(VKRender.deltaTime);
-            ImGui.Text($"{VKRender.deltaTime*1000,6:F2}ms | {fps,4}fps");
+            DisplayFps();
             ImGui.ShowDemoWindow();
-            // Console.WriteLine($"{VKRender.deltaTime*1000,6:F2}ms | {fps,4}fps");
             VKRender.Render();
             //input
             //systems
@@ -67,6 +67,48 @@ public static class Game
 
     public static void Start()
     {
+        
+    }
+
+    private static void DisplayFps()
+    {
+        int location = 0;
+        var io = ImGui.GetIO();
+        var flags = ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoNav | ImGuiWindowFlags.AlwaysAutoResize |
+                    ImGuiWindowFlags.NoFocusOnAppearing | ImGuiWindowFlags.NoSavedSettings;
+
+        if (location >= 0)
+        {
+            const float PAD = 10.0f;
+            var viewport = ImGui.GetMainViewport();
+            var work_pos = viewport.WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+            var work_size = viewport.WorkSize;
+            Vector2 window_pos, window_pos_pivot;
+            window_pos.X = (location & 1) != 0 ? (work_pos.X + work_size.X - PAD) : (work_pos.X + PAD);
+            window_pos.Y = (location & 2) != 0 ? (work_pos.Y + work_size.Y - PAD) : (work_pos.Y + PAD);
+            window_pos_pivot.X = (location & 1) != 0 ? 1.0f : 0.0f;
+            window_pos_pivot.Y = (location & 2) != 0 ? 1.0f : 0.0f;
+            
+            ImGui.SetNextWindowPos(window_pos, ImGuiCond.Always, window_pos_pivot);
+            flags |= ImGuiWindowFlags.NoMove;
+        } else if (location == -2)
+        {
+            ImGui.SetNextWindowPos(io.DisplaySize - new Vector2(0.0f, 0.0f), ImGuiCond.Always, new Vector2(1.0f, 1.0f));
+            flags |= ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove;
+        }
+        ImGui.SetNextWindowBgAlpha(0.35f);
+        if (ImGui.Begin("Example: Simple overlay", ImGuiWindowFlags.NoDecoration |
+                                                    ImGuiWindowFlags.AlwaysAutoResize |
+                                                    ImGuiWindowFlags.NoSavedSettings |
+                                                    ImGuiWindowFlags.NoFocusOnAppearing |
+                                                    ImGuiWindowFlags.NoNav))
+        {
+            // TODO: Rightclick to change pos
+            ImGui.Text("Demo Scene");
+            ImGui.Separator();
+            ImGui.Text($"Application average {1000.0f / ImGui.GetIO().Framerate:F3} ms/frame ({ImGui.GetIO().Framerate:F1} FPS)");
+            ImGui.End();
+        }
         
     }
 }
